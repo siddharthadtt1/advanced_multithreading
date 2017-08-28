8118c33e6c1fa122789be62c8043ba2d9a0692d7
siddharthadtt1 <siddharthadtt1@gmail.com>
2017-08-28 13:22:57 +0530

    Added volatile keyword meaning & comparision with synchronized

--- src/org/module1/BasicThreadTest.java
+++ src/org/module1/BasicThreadTest.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 /*
- * volatile keyword : 
+ * volatile keyword : Generally thread's make a copy of the local variables/ cache to optimize the code.
+ * 					  Volatile keyword signifies that the thread calls the main memory to retrieve the variables values.
+ * 					  The differences between volatile & synchronized are: 
+ * 					  i).  Volatile works on both primitive as well as object type.Synchronized works on only object tyoe.
+ * 				 	  ii). Volatile cannot lock the object which the synchronized can do.
+ * 					  iii).Volatile cannot be used if we want to make read-update-write an atomic operation
+ * 					  iv). Volatile object can be null whereas synchronized objects cannot be null, they will throw 
+ * 						   NullPointerExcetion.  
  * 
  */
 public class BasicThreadTest {
