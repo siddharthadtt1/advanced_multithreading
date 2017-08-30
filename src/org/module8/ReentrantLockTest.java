d446148df5b23636f7aa805b228fcea907b8cd1a
Siddhartha Datta <siddharthadtt1@gmail.com>
2017-08-30 19:24:13 +0530

    Added ReentrantLock example

---
+++ src/org/module8/ReentrantLockTest.java
@@ -0,0 +1,70 @@
+package org.module8;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+
+	public static void main(String[] args) {
+		ReentrantLockProcessor processor = new ReentrantLockProcessor();
+
+		Thread threadOne = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				processor.firstMethod();
+			}
+		}, "Thread 1");
+
+		Thread threadTwo = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				processor.secondMethod();
+			}
+		}, "Thread 2");
+
+		threadOne.start();
+		threadTwo.start();
+
+		try {
+			threadOne.join();
+			threadTwo.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		processor.finished();
+
+	}
+
+}
+
+class ReentrantLockProcessor {
+
+	private Lock lock = new ReentrantLock();
+	private int count = 0;
+
+	private void increment() {
+		for (int i = 1; i <= 10000; i++) {
+			count++;
+		}
+	}
+
+	public void firstMethod() {
+		lock.lock();
+		increment();
+		lock.unlock();
+	}
+
+	public void secondMethod() {
+		lock.lock();
+		increment();
+		lock.unlock();
+	}
+
+	public void finished() {
+		System.out.println("Count : " + count);
+	}
+
+}
